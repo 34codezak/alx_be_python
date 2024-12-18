36b17319ec2ff59ff44afcae4302bcb97316d661
34codezak <zacharyz4qke@gmail.com>
2024-12-18 18:29:07 +0300

    Add daily_reminder.py

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,23 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/o): ")
+
+match priority:
+    case "High":
+        priority_message = "This task has high priority."
+    case "Medium":
+        priority_message = "This task has medium priority."
+    case 'Low':
+        priority_message = "This task has low priority."
+    case _:
+        priority_message = "Unknown priority."
+
+# Check if the task is time-bound and modify the reminder accordingly
+if time_bound == 'yes':
+    time_message = "That requires immediate attention today!"
+else:
+    time_message = "No immediate action is required."
+
+# Provide a customized reminder
+print('Task: {task}')
+print(f'{priority_message} {time_message}')
