6168aae3759e06a1546a68bc5d312b01620fe234
34codezak <zacharyz4qke@gmail.com>
2025-01-22 05:46:38 +0300

    Add oop

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -10,7 +10,7 @@ class Calculator:
     # Class Method: multiply
     @classmethod
     def multiply(cls, a, b):
-        print(f"Calculation Type: {cls.calculation_type}")
+        print(f"Calculation type: {cls.calculation_type}")
         return a * b
 
 # Example usage:
